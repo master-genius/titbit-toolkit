d4a99da89c9c9e61d63ec2ae863bccc454b07e5d
wangyong <1146040444@qq.com>
2020-12-26 12:48:11 +0800

    --

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs')
 
-let files = fs.readdirSync(`${__dirname}/middleware/`, {withFileTypes: true})
+let files = fs.readdirSync(`${__dirname}/extends/`, {withFileTypes: true})
 
 for (let i = 0; i < files.length; i++) {
   if (! files[i].isFile()) {
@@ -19,7 +19,7 @@ for (let i = 0; i < files.length; i++) {
 
   let modname = files[i].name.substring(0, files[i].name.length-3)
 
-  exports[modname] = require('./middleware/'+files[i].name)
+  exports[modname] = require('./extends/'+files[i].name)
 
 }
 
