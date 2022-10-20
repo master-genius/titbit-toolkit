80602a5f97ee6bc793c08160040384032a3882f9
Wangyong <1146040444@qq.com>
2022-10-20 17:11:07 +0800

    test for jwt

--- test/test-jwt.js
+++ test/test-jwt.js
@@ -4,16 +4,29 @@ const jwt = require('../extends/jwt')
 
 let j = new jwt()
 
-j.alg = 'hs512'
+j.alg = 'sm3'
 
-let token = j.make({
+let data = {
   id: '123we',
   name: '飞龙在天',
   username: 'long'
-})
+}
 
-console.log(token)
-
+let token = j.make(data)
 let r = j.verify(token)
 
+console.log('SM3: ')
+console.log(token)
 console.log(r)
+
+
+j.alg = 'sha512'
+
+token = j.make(data)
+
+r = j.verify(token)
+
+console.log('SHA512: ')
+console.log(token)
+console.log(r)
+
